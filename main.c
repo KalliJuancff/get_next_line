e61dae6d2459a84d5b8fdaadc7a2d40eaf592565
JC <juancfoofn@yahoo.com>
2024-03-09 13:09:05 +0100

    Update 'main.c': add handle of the lines of the file specified in args, with final count status and update error function handler and exit value when argc != 2

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
-#include <fcntl.h>		// open
+#include <fcntl.h>		// open, O_RDONLY
+#include <stdio.h>		// printf
 #include <string.h>		// strerror
 #include <sys/errno.h>		// errno
-#include <stdio.h>		// printf
 #include <unistd.h>		// close
 #include "get_next_line.h"
 
 
-void mostrar_error(char *motivoError)
+void mostrar_error(char *accionError)
 {
-	printf("Error %s: %d\n", motivoError, errno);
-	printf("%s\n", strerror(errno));
+	printf("Se ha producido un error %s.\n", accionError);
+	printf("Código del error: %d\n", errno);
+	printf("Mensaje de error: \"%s\"\n", strerror(errno));
 }
 
 int main(int argc, char **argv)
@@ -17,11 +18,10 @@ int main(int argc, char **argv)
 	if (argc != 2)
 	{
 		printf("Debe especificar un archivo a abrir.\n");
-		return (0);
+		return (-1);
 	}
 
 	int fd;
-
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
@@ -29,11 +29,20 @@ int main(int argc, char **argv)
 		return (-1);
 	}
 
-	int i = close(0);
-	if (i == -1)
+	int num_lineas = 0;
+	char *next_line;
+	while ((next_line = get_next_line(fd)) != NULL)
+	{
+		printf("Línea %d: '%s'\n", num_lineas + 1, next_line);
+		num_lineas++;
+	}
+	printf("%d líneas procesadas.\n", num_lineas);
+
+	// Desmarcar para forzar un error en el [segundo] close:
+	//     close(fd);
+	if (close(fd) == -1)
 	{
 		mostrar_error("al cerrar el archivo");
 		return (-1);
 	}
 }
-
