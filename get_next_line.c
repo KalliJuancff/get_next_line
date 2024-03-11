7ad3418d7021ff555f1890611634eab8d5988b6f
JC <juancfoofn@yahoo.com>
2024-03-11 18:09:32 +0100

    Update 'get_next_line' function with minimal code to pass test1 (without leaks)

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:10:06 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/03/11 17:54:51 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/03/11 18:03:32 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 char	*get_next_line(int fd)
 {
-	fd++;
-	return "";
+	char	buffer[BUFFER_SIZE + 1];
+	int	bytes_read;
+	static char	*last_line;
+
+	bytes_read = read(fd, buffer, BUFFER_SIZE);
+	buffer[BUFFER_SIZE] = '\0';
+	if (last_line != NULL)
+		free(last_line);
+	if (bytes_read == 0)
+		return (NULL);
+	last_line = ft_strdup(buffer);
+	return (last_line);
 }
