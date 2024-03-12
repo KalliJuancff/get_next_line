aecb9d040c83db7cfa2f63db7eb89d2cc0452f21
JC <juancfoofn@yahoo.com>
2024-03-12 13:30:30 +0100

    Update 'assertions.c' for replace intros by linux-style character '$'

--- assertions.c
+++ assertions.c
@@ -1,8 +1,35 @@
+#include "get_next_line.h"
 #include "assertions.h"
+
+void reemplazarIntros(char *s)
+{
+	if (s == NULL)
+		return;
+
+	while (*s != '\0')
+	{
+		if (*s == '\n')
+			*s = '$';
+		s++;
+	}
+}
+
+char *duplicar_cadena_sin_intros(char *cadena)
+{
+	char *result;
+
+	if (cadena == NULL)
+		return (NULL);
+
+	result = ft_strdup(cadena);
+	reemplazarIntros(result);
+	return (result);
+}
 
 void assertEqualString(char *s1, char *s2)
 {
 	int error = 0;
+	char *dup_s1, *dup_s2;
 
 	if (s1 == NULL && s2 != NULL)
 		error = 1;
@@ -13,8 +40,16 @@ void assertEqualString(char *s1, char *s2)
 	else if (strcmp(s1, s2) != 0)
 		error = 1;
 
+	dup_s1 = duplicar_cadena_sin_intros(s1);
+	dup_s2 = duplicar_cadena_sin_intros(s2);
+
 	if (error)
-		printf("%s: Las cadenas '%s' y '%s' *NO* son iguales.\n", FAIL("KO"), s1, s2);
+		printf("%s: Las cadenas '%s' y '%s' *NO* son iguales.\n", FAIL("KO"), dup_s1, dup_s2);
 	else
-		printf("%s: Las cadenas '%s' y '%s' son iguales.\n", SUCCESS("OK"), s1, s2);
+		printf("%s: Las cadenas '%s' y '%s' son iguales.\n", SUCCESS("OK"), dup_s1, dup_s2);
+
+	if (dup_s1 != NULL)
+		free(dup_s1);
+	if (dup_s2 != NULL)
+		free(dup_s2);
 }
