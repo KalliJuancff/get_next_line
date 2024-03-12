ebcf0cdf74cd7828810746b13aab747f36945fb9
JC <juancfoofn@yahoo.com>
2024-03-12 11:47:50 +0100

    Update 'tests.c' for not require 'real' text files, reading input from stdin instead. Therefore '1.txt' and '2.txt' were removed

--- tests.c
+++ tests.c
@@ -2,24 +2,53 @@
 #include "get_next_line.h"
 #include "assertions.h"
 
+void simular_escritura_desde_teclado(char *texto)
+{
+	int pipefd[2];
+
+	// Creamos un pipe
+	if (pipe(pipefd) == -1)
+	{
+		perror("Error al ejecutar el comando 'pipe'.\n");
+		exit (-1);
+	}
+
+	int len = ft_strlen(texto);
+	// Escribimos en el extremo de lectura del pipe el texto que corresponda
+	write(pipefd[1], texto, len);
+
+	// Redirigimos la entrada estándar al extremo de lectura del pipe
+	if (dup2(pipefd[0], STDIN_FILENO) == -1)
+	{
+		perror ("Error al ejecutar la función 'dup2'.\n");
+		exit (-1);
+	}
+
+	// Cerramos el extremo de escritura del pipe, ya que no lo necesitamos
+	close(pipefd[1]);
+}
+
 void test1()
 {
-	int fd = open("1.txt", O_RDONLY);
+	// ¡Ya no necesito ficheros reales!
+	//     int fd = open("1.txt", O_RDONLY);
+	simular_escritura_desde_teclado("12345");
 
-	assertEqualString(get_next_line(fd), "12345");
-	assertEqualString(get_next_line(fd), NULL);
+	assertEqualString(get_next_line(STDIN_FILENO), "12345");
+	assertEqualString(get_next_line(STDIN_FILENO), NULL);
 
-	close(fd);
+	// close(fd);
 }
 
 void test2()
 {
-	int fd = open("2.txt", O_RDONLY);
+	// int fd = open("2.txt", O_RDONLY);
+	simular_escritura_desde_teclado("123");
 
-	assertEqualString(get_next_line(fd), "123");
-	assertEqualString(get_next_line(fd), NULL);
+	assertEqualString(get_next_line(STDIN_FILENO), "123");
+	assertEqualString(get_next_line(STDIN_FILENO), NULL);
 
-	close(fd);
+	// close(fd);
 }
 
 int main()
