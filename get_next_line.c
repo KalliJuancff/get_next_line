8a73270465d95cad265002abdfd0a681a413acf4
JC <juancfoofn@yahoo.com>
2024-03-13 17:36:10 +0100

    Modify 'get_next_line' function implementation. This implementation will must be refactored, but not only first 3 tests pass; already 4th test pass too

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:10:06 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/03/13 13:31:58 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/03/13 17:23:27 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,55 @@
 
 char	*get_next_line(int fd)
 {
-	static char	*buffer;
+	static char	*buffer = NULL;
 	int			bytes_read;
+	char		*pending;
+	char		*temp;
 
-	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+	pending = NULL;
 	if (buffer == NULL)
-		return (NULL);
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
 	{
-		free(buffer);
-		return (NULL);
+		buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		if (buffer == NULL)
+			return (NULL);
 	}
-	return (buffer);
+	while (1)
+	{
+		bytes_read = read(fd, buffer, BUFFER_SIZE);
+		buffer[bytes_read] = '\0';
+		if (bytes_read == -1)
+		{
+			if (pending != NULL)
+				free(pending);
+			free(buffer);
+			return (NULL);
+		}
+		if (bytes_read == 0)
+		{
+			if (pending == NULL)
+				return (NULL);
+			else
+			{
+				return (pending);
+			}
+		}
+		if (bytes_read < BUFFER_SIZE)
+			if (pending == NULL)
+				return (ft_strdup(buffer));
+			else
+			{
+				temp = ft_strjoin(pending, buffer);
+				free(pending);
+				pending = temp;
+			}
+		else
+			if (pending == NULL)
+				pending = ft_strdup(buffer);
+			else
+			{
+				temp = ft_strjoin(pending, buffer);
+				free(pending);
+				pending = temp;
+			}
+	}
 }
