9e0b05acb92d18ea661abe345d01ab23297764bb
JC <juancfoofn@yahoo.com>
2024-03-15 17:21:31 +0100

    PRB FIXED in 'get_next_line' function: remove 'free(buffer)' call when bytes_read <= 0

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:10:06 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/03/15 17:12:31 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/03/15 17:18:11 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,10 +25,7 @@ char	*get_next_line(int fd)
 	}
 
 	if (bytes_read <= 0)
-	{
-		free (buffer);
 		return (NULL);
-	}
 
 	return (buffer);
 }
