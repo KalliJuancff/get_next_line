d3a3a99f8b98ca823bea5b0085635f2bd9a31797
JC <juancfoofn@yahoo.com>
2024-03-15 17:14:02 +0100

    Test1 pass: minimal changes in 'get_next_line.c' to pass this test

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:10:06 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/03/15 16:45:41 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/03/15 17:12:31 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,21 @@
 
 char	*get_next_line(int fd)
 {
-	fd++;
-	return ft_strdup("");
+	static char	*buffer = NULL;
+	ssize_t			bytes_read;
+
+	if (buffer == NULL)
+		buffer = ft_calloc(BUFFER_SIZE, sizeof(char));
+
+	while ((bytes_read = read(fd, buffer, BUFFER_SIZE)) == BUFFER_SIZE)
+	{
+	}
+
+	if (bytes_read <= 0)
+	{
+		free (buffer);
+		return (NULL);
+	}
+
+	return (buffer);
 }
