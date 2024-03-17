e9c76222a645d07bf45df3dc4a0b0cdf18982513
JC <juancfoofn@yahoo.com>
2024-03-17 12:35:44 +0100

    Change SUT ('get_next_line' function) to pass new test 1c ("1234567890"): requires add buffer to remainder inside read loop

--- get_next_line.c
+++ get_next_line.c
@@ -6,27 +6,60 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:10:06 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/03/17 11:16:11 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/03/17 12:26:55 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+void	add_buffer_to_remainder(char **rem, char *buf)
+{
+	char	*temp;
+
+	if (*rem == NULL && ft_strlen(buf) > 0)
+		*rem = ft_strdup(buf);
+	else
+	{
+		if (ft_strlen(buf) == 0)
+			return ;
+		temp = ft_strjoin(*rem, buf);
+		free (*rem);
+		*rem = temp;
+	}
+}
+
+void	free_and_reset_buffer(char **buf)
+{
+	free(*buf);
+	*buf = NULL;
+}
+
 char	*get_next_line(int fd)
 {
 	static char	*buffer = NULL;
 	char		*remainder;
+	int			end_of_file;
+	ssize_t		bytes_read;
 
 	remainder = NULL;
-	if (buffer == NULL)
-		buffer = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
-	while ((read (fd, buffer, BUFFER_SIZE)) == BUFFER_SIZE)
+	buffer = ft_calloc (BUFFER_SIZE + 1, sizeof(char));
+	end_of_file = 0;
+	while (end_of_file == 0)
 	{
+		if (end_of_file == 0)
+		{
+			bytes_read = read (fd, buffer, BUFFER_SIZE);
+			buffer[bytes_read] = '\0';
+		}
+		add_buffer_to_remainder(&remainder, buffer);
+		if (bytes_read < BUFFER_SIZE)
+			end_of_file = 1;
 	}
-	if (*buffer == '\0')
+	if (*buffer == 0 && remainder == NULL)
+	{
+		free_and_reset_buffer(&buffer);
 		return (NULL);
-	remainder = ft_strdup(buffer);
-	free (buffer);
-	buffer = NULL;
+	}
+	free_and_reset_buffer(&buffer);
 	return (remainder);
 }
