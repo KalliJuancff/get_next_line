7e736ee6e5c287331d62acc5fcacb639b5e9a597
JC <juancfoofn@yahoo.com>
2024-03-18 19:50:16 +0100

    Rewrite 'get_next_line.c'

--- get_next_line.c
+++ get_next_line.c
@@ -6,108 +6,116 @@
 /*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 17:10:06 by jfidalgo          #+#    #+#             */
-/*   Updated: 2024/03/18 10:35:22 by jfidalgo         ###   ########.fr       */
+/*   Updated: 2024/03/18 19:36:34 by jfidalgo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	add_buffer_to_remainder(char **rem, char *buf)
-{
-	char	*temp;
-
-	if (ft_strlen(buf) == 0)
-		return ;
-	if (*rem == NULL)
-		*rem = ft_strdup(buf);
-	else
-	{
-		temp = ft_strjoin(*rem, buf);
-		free (*rem);
-		*rem = temp;
-	}
-}
-
-void	free_and_reset_buffer(char **buf)
-{
-	free(*buf);
-	*buf = NULL;
-}
-
-int	get_intro_pos(char *buf)
+char	*join_buffers(char **buffer, char *buffer2, int bytes)
 {
 	char	*result;
 
-	result = ft_strchr(buf, '\n');
+	buffer2[bytes] = '\0';
+	result = ft_strjoin(*buffer, buffer2);
 	if (result == NULL)
-		return (-1);
-	return (result - buf);
-}
-
-char	*create_substr(char *str, int count, char *prefix)
-{
-	char	*result;
-	size_t	len_prefix;
-
-	if (prefix == NULL)
-		prefix = "";
-	len_prefix = ft_strlen(prefix);
-	result = malloc((len_prefix + count + 1) * sizeof(char));
-	ft_strlcpy(result, prefix, len_prefix + 1);
-	ft_strlcpy(result + len_prefix, str, count + 1);
+		return (NULL);
+	free(*buffer);
+	*buffer = result;
 	return (result);
 }
 
-char	*create_line_with_remainder_and_buffer(char **buf, char **rem)
+char	*remove_from_buffer(char **buf, int count)
 {
-	char	*line;
-	char	*temp;
+	char	*result;
+	int		len;
 
-	line = create_substr(*buf, get_intro_pos(*buf) + 1, *rem);
-	free(*rem);
-	temp = *buf;
-	if (temp[get_intro_pos(*buf) + 1] == '\0')
-		free_and_reset_buffer(buf);
+	len = ft_strlen(*buf);
+	result = ft_calloc(len - count + 1, sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	ft_strlcpy(result, *buf + count, len - count + 1);
+	free(*buf);
+	*buf = result;
+	return (result);
+}
+
+char	*get_next_line_from_buffer(char **buf, char *intro_pos)
+{
+	char	*next_line;
+	int		len;
+
+	if (intro_pos == NULL)
+		len = ft_strlen(*buf);
+	else
+		len = intro_pos - *buf + 1;
+	next_line = ft_calloc(len + 1, sizeof(char));
+	if (next_line == NULL)
+		return (NULL);
+	ft_strlcpy(next_line, *buf, len + 1);
+	if ((*buf)[len] == '\0')
+	{
+		free(*buf);
+		*buf = NULL;
+	}
 	else
 	{
-		*rem = ft_strdup(*buf + get_intro_pos(*buf) + 1);
-		free_and_reset_buffer(buf);
-		*buf = *rem;
+		if (!remove_from_buffer(buf, len))
+		{
+			free(next_line);
+			return (NULL);
+		}
 	}
-	return (line);
+	return (next_line);
+}
+
+char	*get_next_line_from_fd(char **buf, int fd)
+{
+	char	*read_buf;
+	char	*next_line;
+	int		bytes;
+
+	next_line = ft_strchr(*buf, '\n');
+	if (next_line == NULL)
+	{
+		read_buf = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		if (read_buf == NULL)
+			return (NULL);
+		bytes = 1;
+		while (bytes > 0 && next_line == NULL)
+		{
+			bytes = read(fd, read_buf, BUFFER_SIZE);
+			if (bytes < 0 || (bytes && !join_buffers(buf, read_buf, bytes)))
+				return (free(read_buf), NULL);
+			next_line = ft_strchr(*buf, '\n');
+		}
+		free(read_buf);
+		if (bytes == 0 && **buf == '\0')
+			return (NULL);
+		return (get_next_line_from_buffer(buf, next_line));
+	}
+	else
+		return (get_next_line_from_buffer(buf, next_line));
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer = NULL;
-	char		*remainder;
-	int			end_of_file;
-	ssize_t		bytes_read;
+	char		*next_line;
 
-	remainder = NULL;
-	if (buffer != NULL)
-	{
-		remainder = ft_strdup(buffer);
-		free_and_reset_buffer(&buffer);
-	}
-	buffer = malloc ((BUFFER_SIZE + 1) * sizeof(char));
-	end_of_file = 0;
-	while (end_of_file == 0)
-	{
-		bytes_read = read(fd, buffer, BUFFER_SIZE);
-		buffer[bytes_read] = '\0';
-		if (bytes_read < BUFFER_SIZE)
-			end_of_file = 1;
-		if (get_intro_pos(buffer) >= 0)
-			return (create_line_with_remainder_and_buffer(&buffer, &remainder));
-		else
-			add_buffer_to_remainder(&remainder, buffer);
-	}
-	if (*buffer == '\0' && remainder == NULL)
-	{
-		free_and_reset_buffer(&buffer);
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	if (buffer == NULL)
+	{
+		buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
+		if (buffer == NULL)
+			return (NULL);
 	}
-	free_and_reset_buffer(&buffer);
-	return (remainder);
+	next_line = get_next_line_from_fd(&buffer, fd);
+	if (next_line == NULL)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+	return (next_line);
 }
