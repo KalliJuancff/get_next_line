48dec84fc839bf71e91779d33aa4b99d51586427
JC <juancfoofn@yahoo.com>
2024-03-31 09:35:03 +0200

    Add 'libft/ft_strjoin.c', modified for not to use 'ft_strlcat' because 'get_next_line_utils.c' can only have 5 functions. 'Normi', are you here?

---
+++ libft/ft_strjoin.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jfidalgo <jfidalgo@student.42bar(...).com  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/08 17:10:25 by jfidalgo          #+#    #+#             */
+/*   Updated: 2024/03/21 16:34:23 by jfidalgo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*result;
+	size_t	s1_len;
+	size_t	s2_len;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	s1_len = ft_strlen(s1);
+	s2_len = ft_strlen(s2);
+	result = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
+	ft_strlcpy(result, s1, s1_len + 1);
+	ft_strlcpy(result + s1_len, s2, s2_len + 1);
+	return (result);
+}
